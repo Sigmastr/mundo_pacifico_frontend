559d37a23e7e2775377402ec3b289891b9ceaebc
CristianMontt <cristianmonttfierro@gmail.com>
2023-03-29 13:45:31 -0300

    [Update] Ajuste en obtener Calles y estilos.
    Se retoca el método posst de obtener calles, donde según los datos seleccionados en el dropdown y clickeando en el botón filtrar, envía los datos por la api.

--- mundo_frontend/src/App.js
+++ mundo_frontend/src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
-import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import {
+  Button,
+  Card,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@mui/material";
 import React from "react";
 import { DataGrid } from "@mui/x-data-grid";
 function App() {
@@ -13,6 +17,7 @@ function App() {
   const [ciudad, setCiudad] = React.useState([]);
   const [ciudadSeleccionada, setCiudadSeleccionada] = React.useState("");
   const [calle, setCalle] = React.useState("");
+
   const handleChange = (event) => {
     setRegionSeleccionada(event.target.value);
     setProvinciaSeleccionada("");
@@ -29,6 +34,32 @@ function App() {
   };
   const handleChange3 = (event) => {
     setCiudadSeleccionada(event.target.value);
+  };
+  const filtrar = () => {
+    obtenerCalles();
+    console.log(calle);
+  };
+
+  const obtenerCalles = () => {
+    try {
+      fetch(`http://127.0.0.1:8000/api/calle/conditional`, {
+        method: "POST",
+        body: JSON.stringify({
+          id_region: regionSeleccionada,
+          id_provincia: provinciaSeleccionada,
+          id_ciudad: ciudadSeleccionada,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((res) => {
+        res.json().then((data) => {
+          setCalle(data);
+        });
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
   const obtenerRegiones = () => {
     fetch(`http://127.0.0.1:8000/api/region/get`, {
@@ -69,114 +100,126 @@ function App() {
       });
   };
 
-  const obtenerCalles = (id) => {
-    fetch(`http://127.0.0.1:8000/api/calle/get/${id}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((respuesta) => respuesta.json())
-      .then((data) => {
-        setCalle(data);
-      });
-  };
+  // const obtenerCalles = (id) => {
+  //   fetch(`http://127.0.0.1:8000/api/calle/get/${id}`, {
+  //     method: "GET",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   })
+  //     .then((respuesta) => respuesta.json())
+  //     .then((data) => {
+  //       setCalle(data);
+  //     });
+  // };
 
   React.useEffect(() => {
     obtenerRegiones();
   }, []);
   React.useEffect(() => {
     obtenerProvincia(regionSeleccionada);
+    // obtenerCallesProvincias(regionSeleccionada);
   }, [regionSeleccionada]);
 
   React.useEffect(() => {
     obtenerCiudad(provinciaSeleccionada);
   }, [provinciaSeleccionada]);
 
-  React.useEffect(() => {
-    obtenerCalles(ciudadSeleccionada);
-  }, [ciudadSeleccionada]);
+  // React.useEffect(() => {
+  //   obtenerCalles(ciudadSeleccionada);
+  // }, [ciudadSeleccionada]);
 
-  const columns = [{ field: "nombre", headerName: "Nombre", width: 2000 }];
+  const columns = [{ field: "nombre", headerName: "Nombre", width: 400 }];
 
   return (
     <div className="App">
       <div className="contenedor-principal">
         <div className="formulario">
           {/* falta filtrar con un botón, puede ser solo con región o region y provicia o las 3 */}
-          <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-            <InputLabel id="demo-simple-select-standard-label">
-              Región
-            </InputLabel>
-            <Select
-              labelId="demo-simple-select-standard-label"
-              id="demo-simple-select-standard"
-              value={regionSeleccionada}
-              onChange={handleChange}
-              label="Región"
+          <Card style={{ width: "60%", marginLeft: "5%" }}>
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+              <InputLabel id="demo-simple-select-standard-label">
+                Región
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-standard-label"
+                id="demo-simple-select-standard"
+                value={regionSeleccionada}
+                onChange={handleChange}
+                label="Región"
+              >
+                <MenuItem value="">
+                  <em>Seleccione Región</em>
+                </MenuItem>
+                {region.map((e) => {
+                  return (
+                    <MenuItem key={e.id} value={e.id}>
+                      {e.nombre}
+                    </MenuItem>
+                  );
+                })}
+              </Select>
+            </FormControl>
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+              <InputLabel id="demo-simple-select-standard-label">
+                Provincia
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-standard-label"
+                id="demo-simple-select-standard"
+                value={provinciaSeleccionada}
+                onChange={handleChange2}
+                label="Provincia"
+              >
+                <MenuItem value="">
+                  <em>Seleccione una Provincia</em>
+                </MenuItem>
+                {provincia.map((e) => {
+                  return (
+                    <MenuItem key={e.id} value={e.id}>
+                      {e.nombre}
+                    </MenuItem>
+                  );
+                })}
+              </Select>
+            </FormControl>
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+              <InputLabel id="demo-simple-select-standard-label">
+                Ciudad
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-standard-label"
+                id="demo-simple-select-standard"
+                value={ciudadSeleccionada}
+                onChange={handleChange3}
+                label="Ciudad"
+              >
+                <MenuItem value="">
+                  <em>Selecciona una Ciudad</em>
+                </MenuItem>
+                {ciudad.map((e) => {
+                  return (
+                    <MenuItem key={e.id} value={e.id}>
+                      {e.nombre}
+                    </MenuItem>
+                  );
+                })}
+              </Select>
+            </FormControl>
+            <Button style={{ marginTop: "3%" }} onClick={filtrar}>
+              Filtrar
+            </Button>
+          </Card>
+          <div className="grilla">
+            <Card
+              style={{
+                height: 400,
+                width: "60%",
+                marginLeft: "5%",
+              }}
             >
-              <MenuItem value="">
-                <em>Seleccione Región</em>
-              </MenuItem>
-              {region.map((e) => {
-                return (
-                  <MenuItem key={e.id} value={e.id}>
-                    {e.nombre}
-                  </MenuItem>
-                );
-              })}
-            </Select>
-          </FormControl>
-          <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-            <InputLabel id="demo-simple-select-standard-label">
-              Provincia
-            </InputLabel>
-            <Select
-              labelId="demo-simple-select-standard-label"
-              id="demo-simple-select-standard"
-              value={provinciaSeleccionada}
-              onChange={handleChange2}
-              label="Provincia"
-            >
-              <MenuItem value="">
-                <em>Seleccione una Provincia</em>
-              </MenuItem>
-              {provincia.map((e) => {
-                return (
-                  <MenuItem key={e.id} value={e.id}>
-                    {e.nombre}
-                  </MenuItem>
-                );
-              })}
-            </Select>
-          </FormControl>
-          <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-            <InputLabel id="demo-simple-select-standard-label">
-              Ciudad
-            </InputLabel>
-            <Select
-              labelId="demo-simple-select-standard-label"
-              id="demo-simple-select-standard"
-              value={ciudadSeleccionada}
-              onChange={handleChange3}
-              label="Ciudad"
-            >
-              <MenuItem value="">
-                <em>Selecciona una Ciudad</em>
-              </MenuItem>
-              {ciudad.map((e) => {
-                return (
-                  <MenuItem key={e.id} value={e.id}>
-                    {e.nombre}
-                  </MenuItem>
-                );
-              })}
-            </Select>
-          </FormControl>
-        </div>
-        <div className="grilla">
-          <div style={{ height: 400, width: "50%", marginLeft: "5%" }}>
-            <DataGrid rows={calle} columns={columns} />
+              <DataGrid rows={calle} columns={columns} pageSize={10} />
+            </Card>
           </div>
         </div>
       </div>
